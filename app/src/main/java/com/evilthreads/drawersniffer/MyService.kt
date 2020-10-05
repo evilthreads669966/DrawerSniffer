69f7b6b304e0b03292f6e6ecd10de586c3cc92f9
evilthreads669966 <evilthreads669966@gmail.com>
2020-10-05 14:07:12 -0500

    refactor subscribe

--- app/src/main/java/com/evilthreads/drawersniffer/MyService.kt
+++ app/src/main/java/com/evilthreads/drawersniffer/MyService.kt
@@ -4,11 +4,13 @@ import android.util.Log
 import androidx.lifecycle.lifecycleScope
 import com.candroid.bootlaces.BootService
 import com.evilthreads.drawersnifferlib.DrawerSniffer
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
+@ExperimentalCoroutinesApi
 class MyService: BootService(){
     init {
         lifecycleScope.launchWhenCreated {
-            DrawerSniffer.subscribe(this@MyService){ notif ->
+            DrawerSniffer.subscribe{ notif ->
                 Log.d("DRAWER SNIFFER", notif.toString())
             }
         }
